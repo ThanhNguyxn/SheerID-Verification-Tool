b5c3ceb184b4efa59f1c1c9d6a6e7c31a68006ea
ThanhNguyxn <thanhnguyxn@users.noreply.github.com>
2025-12-13 20:41:59 +0700

    Config: Disable backend static serving, update frontend API URL

--- auto-verify-tool/server.js
+++ auto-verify-tool/server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public'));
+
 
 app.post('/api/verify', async (req, res) => {
     const { url, type } = req.body;
